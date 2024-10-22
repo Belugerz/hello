248b2b4b4526293ef48472343f8f3e5025e780a4
Belugerz <wesleypeyfendo@gmail.com>
2024-10-22 12:38:42 +0700

    gay

---
+++ cs50/linkedlistsort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int data;
+    struct node *link;
+
+}node;
+
+typedef struct{
+
+}swap;
+
+typedef struct{}
+
+int main(){
+kelar sudah
+}
